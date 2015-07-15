7834615147d4246e6fcdeb6e0a95a1a33d4125e9
shinima <842351815@qq.com>
2015-07-15 11:58:52 +0800

    Code of connetion to server.

---
+++ app/src/main/java/connection/client/Client.java
@@ -0,0 +1,333 @@
+package connection.client;
+
+import connection.json.JSONArray;
+import connection.json.JSONException;
+import connection.json.JSONObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+public class Client {
+    public static final String SERVER_URL = "http://localhost/java/virtual_wallet/";
+    private static Client singleton;
+
+    private boolean logined;
+    private String username, password;
+    private int userId;
+
+    private Client() {
+        logined = false;
+        username = null;
+        password = null;
+        userId = -1;
+    }
+
+    public static Client getClient() {
+        if (singleton == null) {
+            singleton = new Client();
+        }
+        return singleton;
+    }
+
+    // test function
+    public static void main(String[] args) throws IOException {
+        Client client = Client.getClient();
+//        debug(client.register("du3", "du5"));
+//        debug(client.validate("du5", "du5"));
+//        debug(client.register("yang", "yang"));
+//        client.validate("yang", "yang");
+//        client.getUserInfo();
+        client.validate("shinima", "shinima");
+//        debug(client.getUserBalance());
+        debug(client.createItem("item0", null, "item0 description", 20));
+        debug(client.getItemInfo("item0", null));
+//        debug(client);
+    }
+
+    /**
+     * 注册一个用户
+     *
+     * @param username 用户名
+     * @param password 密码
+     * @return response
+     * @throws IOException
+     */
+    public Response register(String username, String password) throws IOException, JSONException {
+        JSONObject content = packActionData("register",
+                new JSONObject().put("username", username).put("password", password));
+
+        return jsonPost(content, Client.SERVER_URL);
+    }
+
+    /**
+     * 验证用户名/密码是否正确
+     *
+     * @param username 用户名
+     * @param password 密码
+     * @return 如果验证通过, 返回true并设置相应的username,password, 否则返回false
+     * @throws IOException
+     */
+    public boolean validate(String username, String password) throws IOException {
+        JSONObject content = packActionData("validate",
+                new JSONObject().put("username", username).put("password", password));
+
+        Response response = jsonPost(content, Client.SERVER_URL);
+        if (response.getResult().equals("success")) { // 验证通过
+            this.logined = true;
+            this.username = username;
+            this.password = password;
+            this.userId = response.json.getInt("userId");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void logout() {
+        logined = false;
+        username = null;
+        password = null;
+    }
+
+    /**
+     * 获取用户的信息
+     *
+     * @return 如果用户的信息, 如果用户不存在, 返回null
+     * @throws IOException
+     */
+    public Response getUserInfo() throws IOException {
+        if (!logined)
+            return null;
+        JSONObject content = packActionData("getUserInfo",
+                addUserInfo(new JSONObject()));
+
+        return jsonPost(content, Client.SERVER_URL);
+    }
+
+    /**
+     * 获取用户的余额
+     *
+     * @return 出错返回-1,否则返回用户的余额(大于等于0)
+     * @throws IOException
+     */
+    public int getUserBalance(Response response) throws IOException {
+        if (response == null || !response.getResult().equals("success")) {
+            return -1;
+        } else {
+            return response.json.getJSONObject("userInfo").getInt("balance");
+        }
+    }
+
+    public int getUserBalance() throws IOException {
+        return getUserBalance(getUserInfo());
+    }
+
+    /**
+     * 创建物品单
+     */
+    public Response createItem(String rawVal, String hashVal, String description, int price)
+            throws IOException {
+        if (!logined)
+            return null;
+        JSONObject data = addUserInfo(new JSONObject());
+        if (rawVal != null)
+            data.put("rawVal", rawVal);
+        if (hashVal != null)
+            data.put("hashVal", hashVal);
+        data.put("description", description).put("price", price);
+        JSONObject content = packActionData("createItem", data);
+
+        return jsonPost(content, Client.SERVER_URL);
+    }
+
+    /**
+     * 获取物品单的信息
+     */
+    public Response getItemInfo(String rawVal, String hashVal) throws IOException {
+        JSONObject data = new JSONObject();
+        if (rawVal != null)
+            data.put("rawVal", rawVal);
+        if (hashVal != null)
+            data.put("hashVal", hashVal);
+        JSONObject content = packActionData("getItemInfo", data);
+        return jsonPost(content, Client.SERVER_URL);
+    }
+
+    public boolean isLogined() {
+        return logined;
+    }
+
+    public String getUsername() {
+        if (logined)
+            return username;
+        else
+            return null;
+    }
+
+    public String getPassword() {
+        if (logined)
+            return password;
+        else
+            return null;
+    }
+
+    public int getuserId() {
+        if (logined)
+            return userId;
+        else
+            return -1;
+    }
+
+
+    private JSONObject addUserInfo(JSONObject data) {
+        if (logined) {
+            data.put("username", username).put("password", password).put("userId", userId);
+        }
+        return data;
+    }
+
+    public String toString() {
+        return "logined:" + logined + "\nuserId:" +
+                userId + "\nusername:" + username + "\npassword:" + password;
+    }
+
+    // debug function
+    static void debugToFile(String content) throws IOException {
+//        System.out.println(content);
+        FileWriter writer = new FileWriter(new File("C:\\Users\\sfc84\\.IdeaIC14\\config\\scratches\\scratch"));
+        writer.write(content);
+        writer.close();
+    }
+
+    // debug function
+    static void debug(Object... objects) {
+        for (Object object : objects) {
+            if (object != null)
+                System.out.print(object.toString() + " ");
+            else
+                System.out.print("null ");
+        }
+        System.out.println();
+    }
+
+    // test function
+    static void test1() throws IOException {
+        Client client = Client.getClient();
+        JSONObject data = new JSONObject();
+        data.put("aaa", "bbb").put("hello", "world");
+        JSONArray array1 = new JSONArray();
+        array1.put(1).put(2).put(3);
+        data.put("array", array1);
+        System.out.println(data.toString());
+        System.out.println(client.jsonPost(data, Client.SERVER_URL));
+    }
+
+    Response jsonPost(JSONObject jsonObject, String serverUrl) throws IOException {
+        return post(jsonEncode(jsonObject), serverUrl);
+    }
+
+    Response post(String content, String serverUrl) throws IOException {
+        URL url = new URL(serverUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+
+        connection.setRequestMethod("POST");
+        connection.setUseCaches(false);
+        connection.setRequestProperty("Content-type",
+                "application/x-www-form-urlencoded");
+        connection.setRequestProperty("accept", "*/*");
+        connection.setRequestProperty("connection/connect", "Keep-Alive");
+        // connection.connect(); // connection.setDoOutput(true)将调用connect(),故connect()可以省略
+        DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+        out.writeBytes(content);
+        out.flush();
+        out.close();
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        String result = readAllFromReader(reader);
+
+        reader.close();
+        connection.disconnect();
+
+        debugToFile(result);
+        return new Response(result);
+    }
+
+    private static JSONObject packActionData(String action, JSONObject data) throws IOException {
+        return new JSONObject().put("action", action).put("data", data);
+    }
+
+    private static String jsonEncode(JSONObject jsonObject) throws IOException {
+        Iterator<String> keys = jsonObject.keys();
+        String key, seprator = "";
+        Object value;
+        StringBuilder result = new StringBuilder();
+        while (keys.hasNext()) {
+            key = keys.next();
+            value = jsonObject.get(key);
+            result.append(seprator).append(urlEncode(key))
+                    .append("=").append(urlEncode(value.toString()));
+            seprator = "&";
+        }
+        return result.toString();
+    }
+
+    private static String urlEncode(String string) throws IOException {
+        return URLEncoder.encode(string, "utf-8");
+    }
+
+    private String readAllFromReader(BufferedReader reader) {
+        String line;
+        StringBuilder builder = new StringBuilder();
+        try {
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
+}
+
+class Response {
+    private String result, errorMsg, rawString;
+    public JSONObject json;
+
+    public Response(String string) {
+        rawString = string;
+        try {
+            json = new JSONObject(string);
+            result = json.getString("result");
+            if (result.equals("error")) {
+                errorMsg = json.getString("errorMsg");
+            }
+        } catch (JSONException e) {
+            json = null;
+        }
+    }
+
+    public String getRawString() {
+        return rawString;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public String toString() {
+        if (json != null)
+            return json.toString(4);
+        else
+            return rawString;
+    }
+}
